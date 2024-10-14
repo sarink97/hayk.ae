eaf1a2511e75e630d20706b7598af95ec0da8d63
gitavenian <gitaravenian@gmail.com>
2024-10-14 17:50:56 +0300

    Two Sections with their content

--- src/app/components/organisms/Section.tsx
+++ src/app/components/organisms/Section.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { Fragment, ReactElement } from 'react'
 import CustomCard from '../molecules/CustomCard'
 import CustomSeparator from '../atoms/CustomSeparator'
 import { TicketIcon } from 'lucide-react'
@@ -10,20 +10,21 @@ interface sectionProps{
 const Section = (props : sectionProps) => {
     const cardArray = props.cardArray;
     return (
-        <div>
-            <h1 className='text-cerise text-[32px] text-center font-bold p-[20px]'>Service</h1>
+        <div className='border-t-[2px] border-b-[2px] border-prussianBlue
+                        p-[10px]'>
+            <h1 className='text-cerise text-[32px] text-center font-bold p-[20px]'>{props.title}</h1>
             <div className="relative w-[1000px] max-w-full h-auto overflow-x-auto 
                             scrollbar-thin scrollbar-thumb-cerise scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full m-auto
                             scrollbar-w-[200px]">
                 <div className="relative w-[1000px] max-w-full h-auto overflow-x-auto 
                 scrollbar-thin scrollbar-thumb-cerise scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full m-auto
                 scrollbar-w-[200px]">
-                    <div className="flex space-x-10 mb-[50px] px-4">
+                    <div className={`flex ${cardArray.length < 4 && 'justify-center'} space-x-10 mb-[50px] px-4`}>
                         {cardArray.map((card, index) => (
-                            <React.Fragment key={index}>
+                            <Fragment key={index}>
                                 <CustomCard title={card.title} description={card.description} icon={card.icon} logo ={props.logo} />
                                 {index < cardArray.length - 1 && <CustomSeparator />}
-                            </React.Fragment>
+                            </Fragment>
                         ))}
                     </div>
                 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
 
-import { ReactElement } from "react";
-import CenterLayout from "./components/atoms/centerLayout";
-import CustomSeparator from "./components/atoms/CustomSeparator";
-import CustomCard from "./components/molecules/CustomCard";
-import Section from "./components/organisms/Section";
-import { TicketIcon } from "lucide-react";
-import CustomImage from "./components/atoms/CustomImage";
+
+import ApricodeSection from "./components/sections/ApricodeSection";
+import Branches from "./components/sections/branches";
 
 
 export default function Home() {
-  const cardArray: Array<{ title: string; icon: ReactElement; description: string; }> = [
-    { title: 'Title1', icon: <TicketIcon />, description: 'Description1' },
-    { title: 'Title2', icon: <TicketIcon />, description: 'Description1' },
-    { title: 'Title3', icon: <TicketIcon />, description: 'Description1' },
-    { title: 'Title3', icon: <TicketIcon />, description: 'Description3' },
-    { title: 'Title4', icon: <TicketIcon />, description: 'Description1' },
-];
   return (
-      <Section cardArray={cardArray} title={""} logo={false}/>
+      <>
+        <Branches />
+        <ApricodeSection />
+      </>
   );
 }
