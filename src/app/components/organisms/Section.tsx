ad5e6f5a9580d9f0cf6456c191eeea841ca94df5
gitavenian <gitaravenian@gmail.com>
2024-10-17 15:55:27 +0300

    Section change

--- src/app/components/organisms/Section.tsx
+++ src/app/components/organisms/Section.tsx
@@ -1,55 +1,41 @@
-import React, { Fragment, ReactElement } from "react";
-import CustomCard from "../molecules/CustomCard";
-import CustomSeparator from "../atoms/CustomSeparator";
-import { TicketIcon } from "lucide-react";
-import HorizontalScroller from "@/app/components/atoms/HorizontalScroller";
-import { StaticImageData } from "next/image";
-interface sectionProps {
-  cardArray: Array<{
+import React, { ReactElement } from 'react';
+import CustomCard from '../molecules/CustomCard';
+import CustomSeparator from '../atoms/CustomSeparator';
+import HorizontalScroller from '@/app/components/atoms/HorizontalScroller';
+import { StaticImageData } from 'next/image';
+
+interface SectionProps {
+    cardArray: Array<{ 
+        title: string; 
+        icon?: ReactElement;
+        logo?: StaticImageData;
+        description: string; 
+        link?: string 
+    }>;
     title: string;
-    icon?: ReactElement;
-    logo?: StaticImageData;
-    description: string;
-  }>;
-  title: string;
-  logo: boolean;
+    logo: boolean;
 }
-const Section = (props: sectionProps) => {
+
+const Section = (props: SectionProps) => {
   const cardArray = props.cardArray;
+
   return (
-    <div
-      
-      className="border-t-[2px] border-b-[2px] border-prussianBlue
-                        py-[10px]"
-    >
-      <h1 className="text-cerise text-[40px] text-center font-bold p-[20px] pb-[0px]">
-        {props.title}
-      </h1>
-      <div
-        className="relative  max-w-full h-auto overflow-x-auto 
-                            scrollbar-thin scrollbar-thumb-cerise scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full m-auto
-                            scrollbar-w-[200px] "
-      >
-        <div
-          className="relative w-full h-auto overflow-x-auto 
-                scrollbar-thin scrollbar-thumb-cerise  scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full m-auto
-                scrollbar-w-[200px]"
-        >
-          <HorizontalScroller
-            width={""}
-            isScrolling={cardArray.length >= 4 ? true : false}
-          >
+    <div className='border-t-[2px] border-b-[2px] border-prussianBlue p-[10px]'>
+      <h1 className='text-cerise text-[40px] text-center font-bold p-[20px] pb-[0px]'>{props.title}</h1>
+      <div className="relative max-w-full h-auto overflow-x-auto scrollbar-thin scrollbar-thumb-cerise scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full m-auto scrollbar-w-[200px]">
+        <div className="relative w-full h-auto overflow-x-auto scrollbar-thin scrollbar-thumb-cerise scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full m-auto scrollbar-w-[200px]">
+          <HorizontalScroller width={''} isScrolling={cardArray.length >= 4}>
             {cardArray.map((card, index) => (
-              <>
-                <CustomCard
-                  key={index}
-                  title={card.title}
-                  description={card.description}
-                  icon={card.icon}
-                  logo={card.logo}
+              <React.Fragment key={index}>
+                <CustomCard 
+                  title={card.title} 
+                  description={card.description} 
+                  icon={card.icon} 
+                  logo={card.logo} 
+                  link={card.link} 
                 />
                 {index < cardArray.length - 1 && <CustomSeparator />}
-              </>
+              </React.Fragment>
             ))}
           </HorizontalScroller>
         </div>
