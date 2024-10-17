1d863830dae1e18ca2264da454b98ba8cd6f8691
benon <beniamin99king@gmail.com>
2024-10-17 16:48:25 +0300

    mainbenon

--- src/components/molecules/FooterLinks.tsx
+++ src/components/molecules/FooterLinks.tsx
@@ -9,9 +9,9 @@ const FooterLinks = () => {
       <div className="absolute top-0 right-0 w-[100%] h-full bg-[#414042] skew-y-2 transform origin-bottom-right border-t-2 border-[#E12454]"></div>
 
       {/* Content inside the footer */}
-      <div className="relative mx-auto px-6 flex gap-5 justify-between w-[800px]">
+      <div className="relative mx-auto px-6 flex gap-5 justify-between w-[400px]">
         <FooterCol
-          title="Our Services"
+          title="Company"
           links={[
             { title: "Home", href: "#home" },
             { title: "About us", href: "#about-us" },
@@ -19,16 +19,16 @@ const FooterLinks = () => {
             { title: "Features", href: "#Features" },
           ]}
         />
-        {/* <FooterCol
-          title="Quick Link"
+        <FooterCol
+          title="Our Services"
           links={[
-            { title: "Knowledge Base", href: "" },
-            { title: "Hire An Expert", href: "/about_us" },
-            { title: "FAQ", href: "/about_us" },
-            { title: "Contect", href: "/about_us" },
+            { title: "Marhaba Armenia", href: "https://marhabaarmenia.com/" },
+            { title: "Marhaba Georgia", href: "https://marhabageorgia.com/" },
+            { title: "Business Formation", href: "#home" },
+            { title: "Apricodes", href: "#branches" },
           ]}
         />
-        <FooterCol
+        {/* <FooterCol
           title="Resourcess"
           links={[
             { title: "Knowledge Base", href: "" },
