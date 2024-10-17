ec33c157714f5c855ce0cf55b2905d59083b2b9f
benon <beniamin99king@gmail.com>
2024-10-17 10:58:48 +0300

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import HomeNav from "@/components/atoms/HomeNav";
+import Footer from "@/components/organism/Footer";
 
 export const metadata: Metadata = {
   title: "Hayk",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <HomeNav />
         <div>{children}</div>
+        <Footer />
       </body>
     </html>
   );
